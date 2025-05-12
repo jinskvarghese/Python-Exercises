c50df078992c7f59c90c086976d8411c16bf0b55
Jins K. Varghese <127202913+jinskvarghese@users.noreply.github.com>
2025-05-12 15:30:10 +0530

    Delete 01-String-Reversal directory

--- 01-String-Reversal/stringrev.py
+++
@@ -1,9 +0,0 @@
-def reverse(sentence):
-    words=sentence.split()
-    reversed_words = [word[::-1] for word in words]
-    return ' '.join(reversed_words)
-
-
-sentence = "Python is Awesome"
-reversed_words_sentence = reverse(sentence)
-print(reversed_words_sentence)
