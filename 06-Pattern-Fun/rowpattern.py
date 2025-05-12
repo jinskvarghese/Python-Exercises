043c68971674f6e58675910d5b766fbe89a940cf
Jins K. Varghese <127202913+jinskvarghese@users.noreply.github.com>
2025-05-12 15:30:56 +0530

    Delete 06-Pattern-Fun directory

--- 06-Pattern-Fun/rowpattern.py
+++
@@ -1,6 +0,0 @@
-def pattern_fun(row_count):
-    for i in range(1, row_count + 1):
-        print((str(i) + ' ') * (row_count - i + 1))
-
-row_count = int(input("Enter row count: "))
-pattern_fun(row_count)
