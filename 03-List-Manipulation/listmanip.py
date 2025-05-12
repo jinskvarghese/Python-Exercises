8e05163eb84eb0318b0a9dee019b08a8668d6a80
Jins K. Varghese <127202913+jinskvarghese@users.noreply.github.com>
2025-05-12 15:30:34 +0530

    Delete 03-List-Manipulation directory

--- 03-List-Manipulation/listmanip.py
+++
@@ -1,8 +0,0 @@
-def manip(number_list):
-    number_list[:]= [num for num in number_list if num<=50]
-    return number_list
-
-
-number_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-manip_list = manip(number_list)
-print(manip_list)
